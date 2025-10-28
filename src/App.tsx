f8289d2965ae60854ea1c5efa2951019e4ba3243
mhardiansyah <mhardiansyah5766@gmail.com>
2025-10-28 14:35:25 +0700

    test perfomance 19

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { lazy, Suspense } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// lazy import biar gak semua dimuat di awal
-const Index = lazy(() => import("./pages/Index"));
-const NotFound = lazy(() => import("./pages/NotFound"));
-const ProjectsPage = lazy(() => import("./project/page"));
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import ProjectsPage from "./project/page";
 
 const queryClient = new QueryClient();
 
@@ -18,13 +15,12 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
